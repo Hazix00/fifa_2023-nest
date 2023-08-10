0b03e009b630e121a46c931d666865e44384a72b
hamza <taoussi.hamza.dev@gmail.com>
2023-08-10 10:16:26 +0100

    feat: implement PlayersService.create

--- src/modules/players/players.service.ts
+++ src/modules/players/players.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, Logger } from '@nestjs/common';
 import { Player } from '@prisma/client';
 import { PaginationDto } from '../../common/dtos';
 import { PlayerWithFormattedSalary } from './dtos';
 import { PrismaService } from '../../common/services/prisma.service';
+import { CreatePlayerDto } from './dtos/create-player.dto';
 
 @Injectable()
 export class PlayersService {
-
-    constructor(private readonly prisma: PrismaService) {}
+  logger = new Logger(PlayersService.name);
+  constructor(private readonly prisma: PrismaService) {}
   async findAll(
     page: number,
     limit: number,
@@ -31,7 +32,6 @@ export class PlayersService {
 
   private formatSalary(player: Player): string {
     if (player.salary >= 1000000) {
-
       return `${this.toFixedTwo(player.salary / 1000000)} M${player.devise}`;
     } else if (player.salary >= 1000) {
       return `${this.toFixedTwo(player.salary / 1000)} K${player.devise}`;
@@ -42,6 +42,24 @@ export class PlayersService {
 
   private toFixedTwo(salary: number): string {
     const formattedSalary = salary.toFixed(2);
-      return formattedSalary.endsWith('.00') ? formattedSalary.slice(0, -3) : formattedSalary;
+    return formattedSalary.endsWith('.00')
+      ? formattedSalary.slice(0, -3)
+      : formattedSalary;
+  }
+
+  async create(createPlayerDto: CreatePlayerDto): Promise<Player> {
+    let player: Player;
+    try {
+      player = await this.prisma.player.create({
+        data: createPlayerDto,
+      });
+    } catch (e) {
+      if (e.code === 'P2002') {
+        throw new ConflictException('Player already exists');
+      }
+      this.logger.error(e);
+      throw new BadRequestException('Something went wrong');
+    }
+    return player;
   }
 }
