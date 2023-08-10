f09c8f0a01d9d8ffd4c41eb0904d470fed3613f8
hamza <taoussi.hamza.dev@gmail.com>
2023-08-10 10:03:49 +0100

    test: PlayersService.create

--- src/modules/players/dtos/index.ts
+++ src/modules/players/dtos/index.ts
@@ -1,4 +1,5 @@
 //@index('./*', f => `export * from '${f.path}'`)
+export * from './create-player.dto'
 export * from './player.dto'
 export * from './players-query.dto'
 //@endindex
--- src/modules/players/players.service.spec.ts
+++ src/modules/players/players.service.spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlayersService } from './players.service';
-import { playersStub } from './test/stubs/players.stubs';
+import { playerStub, playersStub } from './test/stubs/players.stubs';
 import { PrismaService } from '../../common/services/prisma.service';
+import { CreatePlayerDto } from './dtos/create-player.dto';
+import { BadRequestException, ConflictException } from '@nestjs/common';
 
 describe('PlayersService', () => {
   let playersService: PlayersService;
@@ -17,6 +19,8 @@ describe('PlayersService', () => {
             player: {
               findMany: jest.fn(),
               count: jest.fn(),
+              create: jest.fn(),
+              findUnique: jest.fn(),
             },
           },
         },
@@ -31,32 +35,36 @@ describe('PlayersService', () => {
     expect(playersService).toBeDefined();
   });
   describe('when findAll is called', () => {
+    it('then findAll should be defined', () => {
+      expect(playersService.findAll).toBeDefined();
+    });
     it('then return paginated football players with formatted salaries', async () => {
       // Use your stub data for the mock implementation
       const mockPlayers = playersStub().slice(0, 6);
       const mockTotal = mockPlayers.length;
-      (prismaService.player.findMany as jest.Mock).mockResolvedValue(mockPlayers);
+      (prismaService.player.findMany as jest.Mock).mockResolvedValue(
+        mockPlayers,
+      );
       (prismaService.player.count as jest.Mock).mockResolvedValue(mockTotal);
-  
+
       const page = 1;
       const limit = 6;
-  
+
       const result = await playersService.findAll(page, limit);
-  
+
       // Assertions
       expect(prismaService.player.findMany).toHaveBeenCalledTimes(1);
       expect(prismaService.player.findMany).toHaveBeenCalledWith({
         skip: (page - 1) * limit,
         take: limit,
-      })
+      });
       expect(prismaService.player.count).toHaveBeenCalledTimes(1);
-
 
       expect(result.data.length).toBe(limit);
       expect(result.page).toBe(page);
       expect(result.limit).toBe(limit);
       expect(result.total).toBe(mockTotal);
-  
+
       // Check that salaries are formatted as expected
       expect(result.data[0].salary).toBe('118 M$');
       expect(result.data[1].salary).toBe('31.20 M£');
@@ -64,7 +72,58 @@ describe('PlayersService', () => {
       expect(result.data[3].salary).toBe('35 M$');
       expect(result.data[4].salary).toBe('23 M€');
       expect(result.data[5].salary).toBe('19.73 M€');
-  
     });
-  })
+  });
+
+  describe('when create is called', () => {
+    it('then create should be defined', () => {
+      expect(playersService.create).toBeDefined();
+    });
+
+    const mockPlayer = playerStub();
+
+    const mockCreatePlayer: CreatePlayerDto = {
+      firstname: mockPlayer.firstname,
+      lastname: mockPlayer.lastname,
+      goal: mockPlayer.goal,
+      salary: mockPlayer.salary,
+      devise: mockPlayer.devise,
+    };
+
+    it('then return the created player', async () => {
+      (prismaService.player.create as jest.Mock).mockResolvedValue(mockPlayer);
+
+      const player = await playersService.create(mockCreatePlayer);
+      // Assertions
+
+      expect(prismaService.player.create).toHaveBeenCalledTimes(1);
+      expect(prismaService.player.create).toHaveBeenCalledWith({
+        data: mockCreatePlayer,
+      });
+      expect(player).toEqual(mockPlayer);
+    });
+
+    it('then throw error if player with the same firstname and lastname already exists', async () => {
+      (prismaService.player.create as jest.Mock).mockRejectedValue({
+        code: 'P2002' 
+      })
+      // Assertions
+      await expect(playersService.create(mockCreatePlayer)).rejects.toThrow(ConflictException);
+
+      expect(prismaService.player.create).toHaveBeenCalledWith({
+        data: mockCreatePlayer,
+      });
+    });
+    it('then throw BadRequestException for other errors', async () => {
+      (prismaService.player.create as jest.Mock).mockRejectedValue(new Error('something went wrong'));
+      // Assertions
+      await expect(playersService.create(mockCreatePlayer)).rejects.toThrow(BadRequestException);
+
+      expect(prismaService.player.create).toHaveBeenCalledWith({
+        data: mockCreatePlayer,
+      });
+    });
+  });
 });
+
+
